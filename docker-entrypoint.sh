06baccf6231c5236554381e3453c3e0ef029433b
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-10-09 22:11:05 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 # set default nginx FQDN
 BASE_URL=${BASE_URL:-'nginx.abcdesktop.svc.cluster.local'}
-
+echo "Running test on BASE_URL=$BASE_URL"
 # run createdesktop_implicit.postman_collection.json
-
-for f in "postman-collections/*.json" do 
+for f in postman-collections/*.json 
+do 
   echo "Running collection $f"
   newman run --env-var "base_url=$BASE_URL" $f
-done"
+done
