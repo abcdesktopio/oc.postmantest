7969cb07d40315cd68feebec77a676a388dca428
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-10-09 16:14:10 +0200

    add multiple file support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
+# set default nginx FQDN
 BASE_URL=${BASE_URL:-'nginx.abcdesktop.svc.cluster.local'}
 
 # run createdesktop_implicit.postman_collection.json
-newman run --env-var "base_url=$BASE_URL" postman-collections/createdesktop_implicit.postman_collection.json
+
+for f in "postman-collections/*.json" do 
+  echo "Running collection $f"
+  newman run --env-var "base_url=$BASE_URL" $f
+done"
