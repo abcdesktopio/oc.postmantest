1898be5ce926557682afecce01ce46b427725497
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-10-09 15:10:05 +0200

    fix base_url

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-BASE_URL=${BASE_URL:-'nginx.abcdesktop.svc.cluser.local'}
+BASE_URL=${BASE_URL:-'nginx.abcdesktop.svc.cluster.local'}
 
 # run createdesktop_implicit.postman_collection.json
 newman run --env-var "base_url=$BASE_URL" postman-collections/createdesktop_implicit.postman_collection.json
